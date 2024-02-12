1000d2144ac627ef4708005546963e6eba8c2914
pzc <927068267@qq.com>
2024-02-12 14:57:05 +0800

    add tss struct

--- include/os.h
+++ include/os.h
@@ -49,4 +49,15 @@ typedef uint32_t page_table_t;
 typedef uint32_t page_dir_t;
 
 
+typedef struct _tss_t {
+    uint32_t pre_link;
+    uint32_t esp0, ss0, esp1, ss1, esp2, ss2;
+    uint32_t cr3;
+    uint32_t eip, eflags, eax, ecx, edx, ebx, esp, ebp, esi, edi;
+    uint32_t es, cs, ss, ds, fs, gs;
+    uint32_t ldt;
+    uint32_t iomap;
+}tss_t;
+
+
 #endif  // OS_H
